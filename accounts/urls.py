b5136f5bd940fd68ada5adc9f7b9988eddb4663f
Leo Meyer <>
2023-09-24 22:12:13 -0500

    Created a registration page

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.registration, name='registration'),
+	path('register/', views.registration, name='registration'),
+	path('confirm/<str:uidb64>/<str:token>/', views.confirm_email, name='confirm_email'),
 ]
-
