a2d7c5719c6b0911474fab92aa28b7375d45a84a
Leo Meyer <24969491+leomeyer1908@users.noreply.github.com>
2023-09-24 22:12:13 -0500

    Created a registration page

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,59 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class RegistrationForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password1', 'password2']
+	name = forms.CharField(max_length=100, required=True, help_text="Required. 100 characters or fewer.")
+	email = forms.EmailField(max_length=254, required=True, help_text="Required. Enter a valid email address.")
+	phone_number = forms.CharField(max_length=20, required=True, help_text="Required. Enter your phone number.")
+	address = forms.CharField(max_length=255, required=True, help_text="Required. Enter your address.")
+
+	dietary_restrictions = forms.CharField(max_length=255, required=False, help_text="Specify dietary restrictions and preferences for dining recommendations.")
+	emergency_contact = forms.CharField(max_length=255, required=False, help_text="Add emergency contact information for safety.")
+	travel_style_preferences = forms.CharField(max_length=255, required=False, help_text="Indicate travel style preferences (e.g., solo, family, adventure).")
+	preferred_airlines = forms.CharField(max_length=255, required=False, help_text="Specify preferred airlines or hotel chains.")
+	budget_constraints = forms.DecimalField(max_digits=10, decimal_places=2, required=False, help_text="Set budget constraints for travel recommendations.")
+
+	preferred_travel_dates = forms.DateField(required=False, help_text="Select preferred travel dates.")
+	trip_duration = forms.IntegerField(required=False, help_text="Specify trip duration in days.")
+	
+	NOTIFICATION_CHOICES = (
+		('email', 'Email'),
+		('sms', 'SMS'),
+		('push', 'Push Notifications'),
+	)
+	notification_preferences = forms.MultipleChoiceField(
+		choices=NOTIFICATION_CHOICES,
+		widget=forms.CheckboxSelectMultiple,
+		required=False,
+		help_text="Choose notification preferences during registration."
+	)
+
+	profile_picture = forms.ImageField(required=False, help_text="Upload a profile picture.")
+
+	profile_privacy = forms.ChoiceField(
+        choices=[('private', 'Private'), ('public', 'Public')],
+        widget=forms.RadioSelect,
+        required=False,
+        help_text="Choose to keep the profile picture private or public."
+    )
+
+	email2 = forms.EmailField(
+		label="Confirm Email",
+		required=True,
+		help_text="Enter the same email address as above, for verification."
+	)
+
+	def clean(self):
+		cleaned_data = super().clean()
+		email = cleaned_data.get('email')
+		email_confirmation = cleaned_data.get('email_confirmation')
+
+		# Check if the email and email confirmation match
+		if email and email_confirmation and email != email_confirmation:
+			raise forms.ValidationError("Email addresses do not match.")
+
+	class Meta:
+		model = User
+		fields = ['username', 'name', 'email', 'email2', 'phone_number', 'address', 'password1', 'password2',
+				  'dietary_restrictions', 'emergency_contact', 'travel_style_preferences',
+				  'preferred_airlines', 'budget_constraints', 'preferred_travel_dates', 'trip_duration',
+				  'notification_preferences', 'profile_picture', 'profile_privacy']
--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,55 @@
 # accounts/views.py
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, get_user_model
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+from django.core.mail import send_mail
+from django.contrib.auth.tokens import default_token_generator
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+
+def send_confirmation_email(request, user):
+	current_site = get_current_site(request)
+	mail_subject = 'Confirm your email address'
+	token = default_token_generator.make_token(user)
+	uid = urlsafe_base64_encode(force_bytes(user.pk))
+	confirmation_url = reverse('confirm_email', args=[uid, token])
+	message = render_to_string('registration/confirmation_email.html', {
+		'user': user,
+		'domain': current_site.domain,
+		'confirmation_url': confirmation_url,
+	})
+	send_mail(mail_subject, message, 'your_email@gmail.com', [user.email])
+	return render(request, 'registration/email_confirmation_sent.html')
+
+def confirm_email(request, uidb64, token):
+	User = get_user_model()
+	try:
+		uid = urlsafe_base64_decode(uidb64).decode()
+		user = User.objects.get(pk=uid)
+	except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+		user = None
+
+	if user and default_token_generator.check_token(user, token):
+		user.email_confirmed = True
+		user.save()
+		return render(request, 'registration/email_confirmed.html')
+	else:
+		return render(request, 'registration/invalid_confirmation_link.html')
 
 def registration(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('home')  # Replace 'home' with the URL name for your home page
-    else:
-        form = RegistrationForm()
-    return render(request, 'registration/registration.html', {'form': form})
+	if request.method == 'POST':
+		form = RegistrationForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			return redirect('home')  # Replace 'home' with the URL name for your home page
+	else:
+		form = RegistrationForm()
+	return render(request, 'registration/registration.html', {'form': form})
 
