8d311a37f777e69ca5975b13d1d68f639bb5e154
Leo Meyer <24969491+leomeyer1908@users.noreply.github.com>
2023-10-08 23:55:31 -0500

    Finished sprint 2 requirements: region selection, tourist attraction recommendation, and schedule generator

--- travel/forms.py
+++ travel/forms.py
@@ -1,10 +1,87 @@
-# travel/forms.py
+"""
+Name of File: travel/forms.py
+Brief description of the file: create the forms forms for all the travel recommendation pages 
+Inputs: None
+Outputs: send the forms to the user for the proper travel recommendation pages
+"""
 from django import forms
-from .models import TravelRegion
 
+#create the forms for the region selection page
 class RegionSelectionForm(forms.Form):
-    region = forms.ModelChoiceField(
-        queryset=TravelRegion.objects.all(),
-        empty_label="Select a Region",
-        widget=forms.Select(attrs={'class': 'form-control'}),
-    )
+	#list of all the options
+	option_list = [
+		"interests", "budget", "climate", "activities",
+		"cultural_characteristics", "attractions", 
+		"transportation_options", "dining_options"
+	]
+	
+	# Create a form field for each of the personalized user settings
+	interests = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+	budget = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+	climate = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+	activities = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+	cultural_characteristics = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+	attractions = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+	transportation_options = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+	dining_options = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+
+
+#create the Meta for the region selection to be able to display the forms
+class RegionSelectionFormMeta:
+	#create Meta class
+    class Meta:
+		#get the fields to be displayed from the region selection form
+        fields = RegionSelectionForm.option_list
+
+#set the region selection meta to be the region selection meta class
+RegionSelectionForm.Meta = RegionSelectionFormMeta
+
+
+#create the forms for the tourist attractions page
+class TouristAttractionForm(forms.Form):
+	#list of all the options
+	option_list = ["interests", "activity_types", "do_you_want_to_visit_museums?", "do_you_want_to_visit_parks?", "do_you_want to_visit_landmarks?", "how_popular_should_the_places_be?", "what_should_the_ratings_be?", "what_proximity_should_they_be_from each_other?"]
+
+	# Create a form field for each of the personalized user settings
+	interests = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+	activity_types = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+	museums = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+	parks = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+	landmarks = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+	popular = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+	ratings = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+	proximity = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+
+
+#create the Meta for the tourist attractions to be able to display the forms
+class TouristAttractionFormMeta:
+	#create Meta class
+    class Meta:
+		#get the fields to be displayed from the tourist attractions form
+        fields = TouristAttractionForm.option_list
+
+#set the region selection meta to be the tourist attractions meta class
+TouristAttractionForm.Meta = TouristAttractionFormMeta
+
+
+
+#create the forms for the generate schedule page page
+class GenerateScheduleForm(forms.Form):
+	#list of all the options
+	option_list = ["date", "time", "duration"]
+
+	# Create a form field for each of the personalized user settings
+	date = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+	time = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+	duration = forms.CharField(max_length=100, required=False, help_text="100 characters or fewer.")
+
+
+#create the Meta for the generate schedule to be able to display the forms
+class GenerateScheduleFormMeta:
+	#create Meta class
+    class Meta:
+		#get the fields to be displayed from the generate schedule form
+        fields = GenerateScheduleForm.option_list
+
+#set the generate schedule meta to be the tourist attractions meta class
+GenerateScheduleForm.Meta = GenerateScheduleFormMeta
--- travel/gpt_integration.py
+++ travel/gpt_integration.py
@@ -1,8 +1,13 @@
+"""
+Name of File: travel/gpt_integration.py
+Brief description of the file: gets information from the user, creates prompts for GPT, and sends back result from GPT model 
+Inputs: information from user
+Outputs: prompt from GPT model
+"""
+
 import openai
-if __name__ != "__main__":
-	from django.conf import settings 
 
-
+#main function is only used for testing, should be ignored otherwise
 def main():
 	option_list = ["interests", "budget", "climate", "activities", "cultural characteristics", "attractions", "transporation options", "dining options"]
 	region_options = {}
@@ -33,50 +38,70 @@ def main():
 	"""
 
 
-	
-
+#creates the prompt for displaying the regions based on the user preferences
 def display_regions(user_options): 
+	#starts prompt by telling GPT to list 3 regions with a criteria in a certain format
 	prompt = """Suggest a list of the best 3 regions to travel to that match with the following criteria in the desired format:
-	Criteria:
-	###"""
+Criteria:
+###"""
+	#for each user preference
 	for option in user_options:
-		prompt += f"{option}: user_options\n"
-	prompt = "###\n"
+		#if the user typed in the preference
+		if user_options[option].strip() != "":
+			#add it to the prompt
+			prompt += f"{option}: {user_options[option]}\n"
+	prompt += "###\n"
 
-	prompt = """Desired Format: ###
+	#add the desired format GPT should use
+	prompt += """Desired Format: 
 	Travel Region 1: <City, Country>, <Explanation/Copywriting>
 	Travel Region 2: <City, Country>, <Explanation/Copywriting>
-	Travel Region 3: <City, Country>, <Explanation/Copywriting>###"""
+	Travel Region 3: <City, Country>, <Explanation/Copywriting>"""
 
+	#return the prompt
 	return prompt
 
 
-		
+#creates the prompt for recommending tourist attractions based on the user preferences and the region
 def recommend_tourist_attraction(region, user_options): 
-	prompt = """Suggest a list of the best 3 tourist attractions in {region} that match with the following criteria:
-	Criteria:
-	###"""
+	#start prompt by telling GPT to list 3 tourist attraction in a region with a criteria in a certain format
+	prompt = f"Suggest a list of the best 3 tourist attractions in {region} that match with the following criteria:"
+	prompt += """Criteria:
+###"""
+	#for each user preference
 	for option in user_options:
-		prompt += f"{option}: user_options\n"
-	prompt = "###\n"
+		#if the user typed in the preference
+		if user_options[option].strip() != "":
+			#add it to the prompt
+			prompt += f"{option}: {user_options[option]}\n"
+	prompt += "###\n"
 
-	prompt = """Desired Format: ###
+	#add the desired format GPT should use
+	prompt += """Desired Format: 
 	Tourist Attraction 1: <Attraction Name> - <Explanation/Copywriting>
 	Tourist Attraction 2: <Attraction Name> - <Explanation/Copywriting>
-	Tourist Attraction 3: <Attraction Name> - <Explanation/Copywriting>###"""
+	Tourist Attraction 3: <Attraction Name> - <Explanation/Copywriting>"""
 
 
+	#return the prompt
 	return prompt
 
+#creates the prompt for recommending tourist attractions based on the user preferences, the region, and attractions list
 def generate_schedule(region, attractions, user_options):
-	prompt = """Based on the following attractions: {", ".join(attractions)} for {region}, create 3 trip schedules that match with the following criteria:
-	Criteria:
+	#start prompt by telling GPT to list 3 schedules in a region with the attractions with a criteria in a certain format
+	prompt = f"Based on the following attractions: {', '.join(attractions)} for {region}, create 3 trip schedules that match with the following criteria:"
+	prompt += """Criteria:
 	###"""
+	#for each user preference
 	for option in user_options:
-		prompt += f"{option}: user_options\n"
-	prompt = "###\n"
+		#if the user typed in the preference
+		if user_options[option].strip() != "":
+			#add it to the prompt
+			prompt += f"{option}: {user_options[option]}\n"
+	prompt += "###\n"
 
-	prompt = """Desired Format: ###
+	#add the desired format GPT should use
+	prompt += """Desired Format:
 	Trip Schedule 1:
 		- 6AM-7AM: <what will be done during this time> 
 		- 8AM-10AM: <what will be done during this time> 
@@ -90,35 +115,41 @@ def generate_schedule(region, attractions, user_options):
 		- <time>: <what will be done during this time> 
 		...
 		- <time>: <what will be done during this time> 
-	###"""
+	"""
 
+	#return the prompt
 	return prompt
 
 	
 	
-
+#generate text from a prompt using GPT
 def generate_text_with_gpt(input_text):
-    #openai.api_key = settings.OPENAI_API_KEY 
+	#openai api key
+	openai.api_key = "" 
+	#create a reponse from GPT
 	response = openai.ChatCompletion.create(
-		model="gpt-3.5-turbo",
+		model="gpt-3.5-turbo", #use GPT 3.5
 		messages=[
 		{
-			"role": "assistant",
-			"content": "Trip Planner. You are a trip planner and organizer. You know the best locations and tourist attractions in the world, and you are able to recommend the best places that someone should go based on their needs for them to have the best travel experience possible."
+			"role": "assistant", #user assistant role since we want it to help us
+			"content": "Trip Planner. You are a trip planner and organizer. You know the best locations and tourist attractions in the world, and you are able to recommend the best places that someone should go based on their needs for them to have the best travel experience possible." #tell it that it is going to be a trip planner
 		},
 		{
-			"role": "user",
-			"content": input_text
+			"role": "user", #this is our role, which is the user giving the message
+			"content": input_text #this is the prompt
 		}
 		],
-		temperature=1,
-		max_tokens=2048,
-		top_p=1,
-		frequency_penalty=0,
-		presence_penalty=0
+		temperature=1, #default variable for GPT which is not relevant for this project
+		max_tokens=2048, #max token GPT is allowed to send back
+		top_p=1, #default variable for GPT which is not relevant for this project
+		frequency_penalty=0, #default variable for GPT which is not relevant for this project
+		presence_penalty=0 # default variable for GPT which is not relevant for this project
 	)
 
+	#send back the contents of the message generated by GPT
 	return response["choices"][0]["message"]["content"]
 
+#if the file is called directly for testing
 if __name__ == "__main__":
+	#call main function
 	main()
