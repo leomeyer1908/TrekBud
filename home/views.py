ccb61429698112242f02185ea78b1c849c4d68d6
Leo Meyer <24969491+leomeyer1908@users.noreply.github.com>
2023-10-22 23:48:59 -0500

    integrated the user database information with the GPT model

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
+"""
+Name of File: home/views.py
+Brief description of the file: set function that executes when loading home page
+Inputs: None
+Outputs: loads home page
+"""
+
 from django.shortcuts import render
 
-# Create your views here.
 
+#function that runs when home page is requested
 def home(request):
-
+    #returns the home.html template
     return render(request, 'home.html', {})
