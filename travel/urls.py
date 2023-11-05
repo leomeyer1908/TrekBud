444b7e0acae46618254c4f24446c661e75eeff32
Leo Meyer <>
2023-11-05 15:42:14 -0600

    sprint 4 changes

--- travel/urls.py
+++ travel/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('select-region/', views.select_region, name='select_region'), #make /travel/select-region call views.select_region function
     path('recommend-attractions/', views.recommend_attractions, name='recommend_tourist_attractions'), #link recommend_attraction url with its function
     path('generate-schedule/', views.make_schedule, name='generate_schedule'), #link generate schedule url with its function
+    path('recommend-restaurants/', views.recommend_restaurants, name='recommend_restaurants'), #link recommend restaurants url with its function
 ]
 
