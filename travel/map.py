fa7803a27b09a671911fe00603f568de0942e5c0
Leo Meyer <24969491+leomeyer1908@users.noreply.github.com>
2023-12-02 20:41:49 -0600

    Integrated map with restaurants and made it so that images displays for attractions and region recommendations

--- travel/map.py
+++ travel/map.py
@@ -8,23 +8,30 @@ Outputs: latidude and logintude for a location
 import googlemaps
 
 def search_location(api_key, location_query):
-    # Initialize the Google Maps client
+    #Initialize the Google Maps client
     gmaps = googlemaps.Client(key=api_key)
 
-    # Perform a Places API text search
+    #Perform a Places API text search
     places_result = gmaps.places(location_query)
 
-    # Extract information about the first result
+    #If the the status of the request was OK and there were at least 1 result
     if places_result['status'] == 'OK' and len(places_result['results']) > 0:
+        #get the first result and save it to place
         place = places_result['results'][0]
-        name = place['name']
+        #get the name of the result
+        name = place['name'] 
+        #get the address if avaialble
         address = place.get('formatted_address', 'Address not available')
+        #get the location which includes both the latitude and longitude
         location = place['geometry']['location']
 
+        #return the name, address, and the latitude, and longitude
         return name, address, location['lat'], location['lng']
+    #if a region is not found, then return None
     else:
         return None
 
+#the following is used for debugging purposes
 if __name__ == "__main__":
     api_key = 'GOOGLE_MAPS_API_KEY'
     location_query = input("Enter the location to search for: ")
